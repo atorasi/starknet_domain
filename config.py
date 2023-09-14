1cdb9a13c4c3ad9bda0683cc61e83efab6711000
shizuoka <121640832+atorasi@users.noreply.github.com>
2023-09-14 18:30:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 STARKNET_RPC = "https://starknet-mainnet.public.blastapi.io"
 
 
-
-SLEEP_ACCS_MIN = 10
-SLEEP_ACCS_MAX = 100
+SLEEP = True              # Спать между аккаунтами? True/False, Да/Нет.
+SLEEP_ACCS_MIN = 10       # Минимум секунд
+SLEEP_ACCS_MAX = 100      # Максимум секунд
 
 
 
