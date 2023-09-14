b99c3a7e63fd76033f0e60d1fe1851d7c2eb4532
shizuoka <121640832+atorasi@users.noreply.github.com>
2023-09-14 18:14:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from client.starknet import StarkNet
 
 
 with open('addresses.txt', 'r') as file:
-    evm_keys = [key.strip() for key in file]
+    addresses = [key.strip() for key in file]
     
 with open('private_keys.txt', 'r') as file:
     stark_keys = [key.strip() for key in file]
 
 async def main():
-    for index, (key, address) in enumerate(zip(evm_keys, stark_keys), start=1):
+    for index, (key, address) in enumerate(zip(stark_keys, addresses), start=1):
         client = StarkNet(index, key, address)
         await client.mint_domain()
 
